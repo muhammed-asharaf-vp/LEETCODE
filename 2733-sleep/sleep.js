6ef940e705989de38349abf3875046a213277834
Mohamed Asharaf vp <mohamedasharafvpp@gmail.com>
2025-10-18 12:06:30 +0530

    Time: 45 ms (58.04%) | Memory: 53.9 MB (38.06%) - LeetSync

--- 2733-sleep/sleep.js
+++ 2733-sleep/sleep.js
@@ -1,13 +1,5 @@
-/**
- * @param {number} millis
- * @return {Promise}
- */
 async function sleep(millis) {
     return new Promise(resolve => setTimeout (resolve,millis))
     
 }
 
-/** 
- * let t = Date.now()
- * sleep(100).then(() => console.log(Date.now() - t)) // 100
- */
